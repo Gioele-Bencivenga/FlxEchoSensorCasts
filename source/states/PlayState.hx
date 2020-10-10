4b33e01041cd1ce9d2ff044ae76c0c209eefdf6c
Gioele <gioelebenci@live.com>
2020-10-10 19:07:47 +0100

    Top down conversion

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -15,6 +15,9 @@ using hxmath.math.Vector2;
 using flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState {
+	/// CONSTANTS
+	public static inline final TILE_SIZE = 32;
+
 	var player:Box;
 	var level_data = [
 		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -28,9 +31,8 @@ class PlayState extends FlxState {
 	override function create() {
 		// First thing we want to do before creating any physics objects is init() our Echo world.
 		FlxEcho.init({
-			width: level_data[0].length * 16, // Make the size of your Echo world equal the size of your play field
-			height: level_data.length * 16,
-			gravity_y: 800
+			width: level_data[0].length * TILE_SIZE, // Make the size of your Echo world equal the size of your play field
+			height: level_data.length * TILE_SIZE,
 		});
 
 		// Normal, every day FlxGroups!
@@ -46,18 +48,18 @@ class PlayState extends FlxState {
 				switch (level_data[j][i]) {
 					case 1:
 						// Just a regular old terrain block
-						var bluebox = new Box(i * 16, j * 16, 16, 16, 0xFF0080FF);
+						var bluebox = new Box(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, 0xFF0080FF);
 						bluebox.add_body({mass: 0}); // We'll pass in body options with mass set to 0 so that it's static
 						bluebox.add_to_group(terrain); // Instead of `group.add(object)` we use `object.add_to_group(group)`
 					case 2:
 						// Orange boxes will act like springs!
-						var orangebox = new Box(i * 16, j * 16, 16, 16, 0xFFFF8000);
-						orangebox.origin.set(8, 16); // We'll set the origin here so that we can animate our orange block later
+						var orangebox = new Box(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, 0xFFFF8000);
+						orangebox.origin.set(TILE_SIZE / 2, TILE_SIZE); // We'll set the origin here so that we can animate our orange block later
 						orangebox.add_body({mass: 0});
 						orangebox.add_to_group(bouncers);
 					case 3:
-						player = new Box(i * 16, j * 16, 8, 12, 0xFFFF004D, true);
-						player.add_body();
+						player = new Box(i * TILE_SIZE, j * TILE_SIZE, Std.int(TILE_SIZE / 2), Std.int(TILE_SIZE - TILE_SIZE / 2), 0xFFFF004D, true);
+						player.add_body({mass: 1, drag_length: 800, max_velocity_length: 200});
 						add(player);
 					default:
 						continue;
@@ -65,10 +67,10 @@ class PlayState extends FlxState {
 			}
 
 		// lets add some ramps too! They'll belong to the same collision group as the blue boxes we made earlier.
-		for (i in 0...8) {
+		/*for (i in 0...8) {
 			var ramp = new Ramp(16, 112 + i * 16, 16 + i * 16, 128 - i * 16, NW);
 			ramp.add_to_group(terrain);
-		}
+		}*/
 
 		// Our first physics listener collides our player with the terrain group.
 		player.listen(terrain);
@@ -83,7 +85,7 @@ class PlayState extends FlxState {
 						// This checks to see if the normal of our collision is pointing downward - you could use it for hop and bop games to see if a player has stomped on an enemy!
 						if (col.normal.dot(Vector2.yAxis).round() == 1) {
 							// set the player's velocity to go up!
-							a.velocity.y = -400;
+							a.velocity.y = -4000;
 							// animate the orange box!
 							var b_object:FlxSprite = cast b.get_object();
 							b_object.scale.y = 1.5;
@@ -112,16 +114,22 @@ class Box extends FlxSprite {
 
 	function controls() {
 		var body = this.get_body();
-		body.velocity.x = 0;
-		if (FlxG.keys.pressed.LEFT)
-			body.velocity.x -= 128;
-		if (FlxG.keys.pressed.RIGHT)
-			body.velocity.x += 128;
-		if (FlxG.keys.justPressed.UP && isTouching(FLOOR))
-			body.velocity.y -= 256;
+		// body.velocity.x = 0; trying to have drag do this
+		// body.velocity.y = 0;
+		if (FlxG.keys.pressed.LEFT || FlxG.keys.pressed.A)
+			body.velocity.x -= 30;
+		if (FlxG.keys.pressed.RIGHT || FlxG.keys.pressed.D)
+			body.velocity.x += 30;
+		if (FlxG.keys.justPressed.UP || FlxG.keys.pressed.W)
+			body.velocity.y -= 30;
+		if (FlxG.keys.justPressed.DOWN || FlxG.keys.pressed.S)
+			body.velocity.y += 30;
 	}
 }
 
+/**
+ * NOT USED at the moment, might break
+ */
 class Ramp extends FlxSprite {
 	public function new(x:Float, y:Float, w:Int, h:Int, d:RampDirection) {
 		trace('$x / $y / $w / $h');
